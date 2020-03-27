5be8506acae3a9cf0f676553fe9d2ae74bfa4f4d
Leostep94 <62735867+Leostep94@users.noreply.github.com>
2020-03-26 23:52:12 -0500

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,17 @@
+"Este es mi primer dia con python"
+2*2
+"2"*4
+a="suma"
+a+a
+
+"""
+"Este es mi primer dia con python"
+'Este es mi primer dia con python'
+>>> 2*2
+4
+>>> "2"*4
+'2222'
+>>> a="suma"
+>>> a+a
+'sumasuma'
+"""

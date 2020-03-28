585c9110e99caec0e8ef130377219e4e642970a3
Leostep94 <62735867+Leostep94@users.noreply.github.com>
2020-03-27 23:12:13 -0500

    Create da2.py

---
+++ da2.py
@@ -0,0 +1,16 @@
+the_list=[1, 2, 3]
+the_list.pop
+the_list.pop(1)
+the_list.append(4)
+
+a_dict = {}
+a_dict["abc"]= "another new string"
+new_abc = {'another':'another string','abc':'a string','yetanother':'an otherstring'}
+
+tup= (
+("uno","uno"),
+("dos","dos")
+)
+
+tup[0]
+tup[0][0]

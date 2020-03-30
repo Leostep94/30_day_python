76c6f43a4e3d8820a2c116d1e14e56b534d21173
Leostep94 <62735867+Leostep94@users.noreply.github.com>
2020-03-30 00:01:17 -0500

    Create day3.py

---
+++ day3.py
@@ -0,0 +1,24 @@
+bag = [10,123,5456,7894,12321,1231,2561]
+len(bag)
+for item in bag:
+	print(item)
+i=0
+for item in bag:
+	i=i+1 
+	print(i)
+
+	for item in bag:
+		if item==10
+			print('yeaah')
+
+while <some condition>== True:
+
+i=10
+while i<11:
+	print("yup")
+	i= i+1
+
+i=0
+while i<100
+	print(i)
+	i = i+1
